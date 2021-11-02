9608957884f55a32359bebe064731c93b92820e2
Victor <victor>
2021-11-02 10:50:33 -0500

    made it to step 5, made the x's work

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,10 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
     var body: some View {
         VStack {
-            Text("Hello, world!")
+            Text("Tic Tac Toe")
                 .padding()
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15, content: {
+                ForEach(0..<9) { index in
+                    ZStack {
+                        Color.blue
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        moves[index] = "X"
+                    }
+                }
+            })
         }
         .preferredColorScheme(.dark)
     }
